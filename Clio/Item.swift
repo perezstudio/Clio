2d87f2cb425affa61566551f980369c6ea68914d
Kevin Perez <kp952547@gmail.com>
2026-01-04 21:35:48 -0600

    Initial Commit

---
+++ Clio/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Clio
+//
+//  Created by Kevin Perez on 1/4/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
